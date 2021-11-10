941b03b17c7052a69c22f60af6eaa22d49d6e225
Chalupa Batman <echartock3@gmail.com>
2021-11-10 17:16:46 -0500

    Improve response types

--- lib/api/contacts.ts
+++ lib/api/contacts.ts
@@ -28,103 +28,115 @@ export class IntercomContactsAPI extends IIntercomAPI {
 	public async create(
 		data: CreateOrUpdateContactModel
 	): Promise<AxiosResponse<ContactModel>> {
-		return await this._client.post<ContactModel, CreateOrUpdateContactModel>(
+		const resp = await this._client.post<ContactModel, CreateOrUpdateContactModel>(
 			'/contacts',
 			data
 		);
+		return resp.data
 	}
 
 	public async delete(
 		id: string
 	): Promise<AxiosResponse<DeleteContactResponse>> {
-		return await this._client.delete<DeleteContactResponse, undefined>(
+		const resp = await this._client.delete<DeleteContactResponse, undefined>(
 			`/contacts/${id}`
 		);
+		return resp.data;
 	}
 
 	public async find(
 		query: SearchById | SearchByEmail | SearchByExternalId
 	): Promise<any> {
-		if (determineIfSearchById(query))
-			return await this._client.get<any, any>(`/contacts/${query.id}`);
-		else if (determineIfSearchByEmail(query))
+		if (determineIfSearchById(query)){
+			const resp = await this._client.get<any, any>(`/contacts/${query.id}`);
+			return resp.data;
+		} else if (determineIfSearchByEmail(query)) {
 			return await this.searchByEmail(query.email);
-		else if (determineIfSearchByExternalId(query))
+		} else if (determineIfSearchByExternalId(query)){
 			return await this.searchByExternalId(query.external_id);
+		}
 	}
 
 	public async findUserByEmail(
 		email: string
 	): Promise<any> {
-		return await this.search({
+		const searchResults = await this.search({
 			query: {
 				operator: "AND",
 				value: [
 					{
-						field: 'email',
-						operator: '=',
+						field: "email",
+						operator: "=",
 						value: email
 					},
 					{
-						field: 'role',
-						operator: '=',
+						field: "role",
+						operator: "=",
 						value: "user"
 					}
 				]
 			}
 		});
+		// Return first (and only) user with queried email
+		return searchResults[0];
 	}
 
 	public async list(starting_after: string): Promise<AxiosResponse<ContactsList>> {
-		return await this._client.get<ContactsList, undefined>('/contacts?starting_after=' + starting_after);
+		const resp = await this._client.get<ContactsList, undefined>('/contacts?starting_after=' + starting_after);
+		return resp.data;
 	}
 
 	public async search(
 		search: IntercomSearch
 	): Promise<AxiosResponse<ContactsList>> {
-		return await this._client.post<ContactsList, IntercomSearch>(
+		const resp = await this._client.post<ContactsList, IntercomSearch>(
 			'/contacts/search',
 			search
 		);
+		return resp.data.data;
 	}
 
 	public async update(
 		id: string,
 		data: CreateOrUpdateContactModel
 	): Promise<AxiosResponse<ContactModel>> {
-		return await this._client.put<ContactModel, CreateOrUpdateContactModel>(
+		const resp = await this._client.put<ContactModel, CreateOrUpdateContactModel>(
 			`/contacts/${id}`,
 			data
 		);
+		return resp.data;
 	}
 
 	public async merge(
 		from: string,
 		into: string
 	): Promise<AxiosResponse<ContactModel>> {
-		return await this._client.post<ContactModel, MergeModel>(
+		const resp = await this._client.post<ContactModel, MergeModel>(
 			`/contacts/merge`,
 			{ from, into }
 		);
+		return resp.data;
 	}
 
 	public async createCompany(
 		data: CreateOrUpdateCompanyModel
 	): Promise<AxiosResponse<CompanyModel>> {
-		return await this._client.post<CompanyModel, CreateOrUpdateCompanyModel>(
+		const resp = await this._client.post<CompanyModel, CreateOrUpdateCompanyModel>(
 			`/companies`,
 			data
 		);
+		return resp.data;
 	}
 
 	public async attachCompanyToContact(
 		contactId: string,
 		companyId: string
 	): Promise<AxiosResponse<CompanyModel>> {
-		return await this._client.post<CompanyModel, Record<string, string>>(
+		const resp = await this._client.post<CompanyModel, Record<string, string>>(
 			`/contacts/${contactId}/companies`,
 			{ id: companyId }
 		);
+		return resp.data
 	}
 
 	private async searchByEmail(email: string) {
