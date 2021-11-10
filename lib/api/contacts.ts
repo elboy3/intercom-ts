0be1316056689c7deaa2d9ee09b750ea37eb3f30
Chalupa Batman <echartock3@gmail.com>
2021-11-10 17:25:19 -0500

    Undo improved response types

--- lib/api/contacts.ts
+++ lib/api/contacts.ts
@@ -28,39 +28,35 @@ export class IntercomContactsAPI extends IIntercomAPI {
 	public async create(
 		data: CreateOrUpdateContactModel
 	): Promise<AxiosResponse<ContactModel>> {
-		const resp = await this._client.post<ContactModel, CreateOrUpdateContactModel>(
+		return await this._client.post<ContactModel, CreateOrUpdateContactModel>(
 			'/contacts',
 			data
 		);
-		return resp.data
 	}
 
 	public async delete(
 		id: string
 	): Promise<AxiosResponse<DeleteContactResponse>> {
-		const resp = await this._client.delete<DeleteContactResponse, undefined>(
+		return await this._client.delete<DeleteContactResponse, undefined>(
 			`/contacts/${id}`
 		);
-		return resp.data;
 	}
 
 	public async find(
 		query: SearchById | SearchByEmail | SearchByExternalId
 	): Promise<any> {
-		if (determineIfSearchById(query)){
-			const resp = await this._client.get<any, any>(`/contacts/${query.id}`);
-			return resp.data;
-		} else if (determineIfSearchByEmail(query)) {
+		if (determineIfSearchById(query))
+			return await this._client.get<any, any>(`/contacts/${query.id}`);
+		else if (determineIfSearchByEmail(query))
 			return await this.searchByEmail(query.email);
-		} else if (determineIfSearchByExternalId(query)){
+		else if (determineIfSearchByExternalId(query))
 			return await this.searchByExternalId(query.external_id);
-		}
 	}
 
 	public async findUserByEmail(
 		email: string
 	): Promise<any> {
-		const searchResults = await this.search({
+		return await this.search({
 			query: {
 				operator: "AND",
 				value: [
@@ -77,66 +73,58 @@ export class IntercomContactsAPI extends IIntercomAPI {
 				]
 			}
 		});
-		// Return first (and only) user with queried email
-		return searchResults[0];
 	}
 
 	public async list(starting_after: string): Promise<AxiosResponse<ContactsList>> {
-		const resp = await this._client.get<ContactsList, undefined>('/contacts?starting_after=' + starting_after);
-		return resp.data;
+		return await this._client.get<ContactsList, undefined>('/contacts?starting_after=' + starting_after);
 	}
 
 	public async search(
 		search: IntercomSearch
 	): Promise<AxiosResponse<ContactsList>> {
-		const resp = await this._client.post<ContactsList, IntercomSearch>(
+		return await this._client.post<ContactsList, IntercomSearch>(
 			'/contacts/search',
 			search
 		);
-		return resp.data.data;
 	}
 
 	public async update(
 		id: string,
 		data: CreateOrUpdateContactModel
 	): Promise<AxiosResponse<ContactModel>> {
-		const resp = await this._client.put<ContactModel, CreateOrUpdateContactModel>(
+		return await this._client.put<ContactModel, CreateOrUpdateContactModel>(
 			`/contacts/${id}`,
 			data
 		);
-		return resp.data;
 	}
 
 	public async merge(
 		from: string,
 		into: string
 	): Promise<AxiosResponse<ContactModel>> {
-		const resp = await this._client.post<ContactModel, MergeModel>(
+		return await this._client.post<ContactModel, MergeModel>(
 			`/contacts/merge`,
 			{ from, into }
 		);
-		return resp.data;
 	}
 
 	public async createCompany(
 		data: CreateOrUpdateCompanyModel
 	): Promise<AxiosResponse<CompanyModel>> {
-		const resp = await this._client.post<CompanyModel, CreateOrUpdateCompanyModel>(
+		return await this._client.post<CompanyModel, CreateOrUpdateCompanyModel>(
 			`/companies`,
 			data
 		);
-		return resp.data;
 	}
 
 	public async attachCompanyToContact(
 		contactId: string,
 		companyId: string
 	): Promise<AxiosResponse<CompanyModel>> {
-		const resp = await this._client.post<CompanyModel, Record<string, string>>(
+		return await this._client.post<CompanyModel, Record<string, string>>(
 			`/contacts/${contactId}/companies`,
 			{ id: companyId }
 		);
-		return resp.data
 	}
 
 	private async searchByEmail(email: string) {
